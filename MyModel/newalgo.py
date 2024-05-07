ec5c5505c91d12903800827b36f44acc355d91e5
Wijesooriya A Layanjali <115726378+Bihari83@users.noreply.github.com>
2024-05-07 15:02:53 +0530

    Updating UnityDisplay.dart

--- MyModel/newalgo.py
+++ MyModel/newalgo.py
@@ -177,9 +177,9 @@
 # model.save('symbol_detection_model.h5')
 
 import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.preprocessing.image import ImageDataGenerator # type: ignore
+from tensorflow.keras.models import Sequential # type: ignore
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense # type: ignore
 
 # Define directories for train, validation, and test data
 train_dir = r'E:\DataSet\FDS\Train'
