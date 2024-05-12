bdb2a3d6ef3735e9b8d21a553a5dc269b6c9cedf
Wijesooriya A Layanjali <115726378+Bihari83@users.noreply.github.com>
2024-05-12 20:44:15 +0530

    Updates

--- final_project/lib/screens/image_picker_screen.dart
+++ final_project/lib/screens/image_picker_screen.dart
@@ -1,5 +1,219 @@
-// ignore_for_file: library_private_types_in_public_api, deprecated_member_use, avoid_print
+// // ignore_for_file: library_private_types_in_public_api, deprecated_member_use, avoid_print
 
+// import 'dart:io';
+// import 'package:final_project/screens/SplashScreen.dart';
+// import 'package:flutter/material.dart';
+// import 'package:image_picker/image_picker.dart';
+// import 'package:http/http.dart' as http;
+
+// class ImagePickerScreen extends StatefulWidget {
+//   const ImagePickerScreen({Key? key}) : super(key: key);
+
+//   @override
+//   _ImagePickerScreenState createState() => _ImagePickerScreenState();
+// }
+
+// class _ImagePickerScreenState extends State<ImagePickerScreen> {
+//   File? _image;
+//   final picker = ImagePicker();
+
+//   Future getImageFromCamera() async {
+//     final pickedFile = await picker.getImage(source: ImageSource.camera);
+
+//     setState(() {
+//       if (pickedFile != null) {
+//         _image = File(pickedFile.path);
+//       } else {
+//         print('No image selected.');
+//       }
+//     });
+//   }
+
+//   Future getImageFromGallery() async {
+//     final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+//     setState(() {
+//       if (pickedFile != null) {
+//         _image = File(pickedFile.path);
+//       } else {
+//         print('No image selected.');
+//       }
+//     });
+//   }
+
+// //   Future<void> uploadImage() async {
+// //     if (_image == null) {
+// //       print('No image selected.');
+// //       return;
+// //     }
+
+// //     final uri = Uri.parse("https://32f9-112-134-33-122.ngrok-free.app/upload");
+// //     final request = http.MultipartRequest("POST", uri);
+// //     request.files.add(await http.MultipartFile.fromPath('image', _image!.path));
+
+// //     try {
+// //       final streamedResponse = await request.send();
+// //       final response = await http.Response.fromStream(streamedResponse);
+
+// //       if (response.statusCode == 200) {
+// //         // Handle success response
+// //         print('Image uploaded successfully.');
+// //       } else {
+// //         // Handle error response
+// //         print('Error uploading image: ${response.body}');
+// //       }
+// //     } catch (e) {
+// //       // Handle any exceptions
+// //       print('Exception uploading image: $e');
+// //     }
+// //   }
+
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return Scaffold(
+// //       appBar: AppBar(
+// //         title: const Text('Pick an Image'),
+// //       ),
+// //       body: Center(
+// //         child: Column(
+// //           mainAxisAlignment: MainAxisAlignment.center,
+// //           children: [
+// //             _image == null
+// //                 ? const Text('No image selected.')
+// //                 : Image.file(_image!),
+// //             if (_image != null) // Conditionally render the upload button
+// //               Row(
+// //                 mainAxisAlignment: MainAxisAlignment.center,
+// //                 children: [
+// //                   ElevatedButton(
+// //                     onPressed: uploadImage,
+// //                     child: const Text('Upload'),
+// //                   ),
+// //                   const SizedBox(width: 10),
+// //                   ElevatedButton(
+// //                     onPressed: () {
+// //                       Navigator.push(
+// //                         context,
+// //                         MaterialPageRoute(
+// //                             builder: (context) =>
+// //                                 const SplashScreen()), // Navigate to SplashScreen
+// //                       );
+// //                     },
+// //                     child: const Text('Develop'),
+// //                   ),
+// //                 ],
+// //               ),
+// //           ],
+// //         ),
+// //       ),
+// //       floatingActionButton: Row(
+// //         mainAxisAlignment: MainAxisAlignment.end,
+// //         children: [
+// //           FloatingActionButton(
+// //             onPressed: getImageFromCamera,
+// //             tooltip: 'Pick Image from Camera',
+// //             child: const Icon(Icons.add_a_photo),
+// //           ),
+// //           const SizedBox(width: 10),
+// //           FloatingActionButton(
+// //             onPressed: getImageFromGallery,
+// //             tooltip: 'Pick Image from Gallery',
+// //             child: const Icon(Icons.photo_library),
+// //           ),
+// //         ],
+// //       ),
+// //     );
+// //   }
+// // }
+//   Future<void> uploadImage() async {
+//     if (_image == null) {
+//       print('No image selected.');
+//       return;
+//     }
+
+//     final uri = Uri.parse(
+//         "https://32f9-112-134-33-122.ngrok-free.app/upload");
+//     final request = http.MultipartRequest("POST", uri);
+//     request.files.add(await http.MultipartFile.fromPath('image', _image!.path));
+
+//     try {
+//       final streamedResponse = await request.send();
+//       final response = await http.Response.fromStream(streamedResponse);
+
+//       if (response.statusCode == 200) {
+//         // Handle success response
+//         print('Image uploaded successfully.');
+//       } else {
+//         // Handle error response
+//         print('Error uploading image: ${response.body}');
+//       }
+//     } catch (e) {
+//       // Handle any exceptions
+//       print('Exception uploading image: $e');
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Pick an Image'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             _image == null
+//                 ? const Text('No image selected.')
+//                 : Image.file(_image!),
+//             if (_image != null) // Conditionally render the upload button
+//               Row(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: [
+//                   ElevatedButton(
+//                     onPressed: uploadImage,
+//                     child: const Text('Upload'),
+//                   ),
+//                   const SizedBox(width: 10),
+//                   ElevatedButton(
+//                     onPressed: () {
+//                       Navigator.push(
+//                         context,
+//                         MaterialPageRoute(
+//                             builder: (context) =>
+//                                 const SplashScreen()), // Navigate to SplashScreen
+//                       );
+//                     },
+//                     child: const Text('Develop'),
+//                   ),
+//                 ],
+//               ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: Row(
+//         mainAxisAlignment: MainAxisAlignment.end,
+//         children: [
+//           FloatingActionButton(
+//             onPressed: getImageFromCamera,
+//             tooltip: 'Pick Image from Camera',
+//             child: const Icon(Icons.add_a_photo),
+//           ),
+//           const SizedBox(width: 10),
+//           FloatingActionButton(
+//             onPressed: getImageFromGallery,
+//             tooltip: 'Pick Image from Gallery',
+//             child: const Icon(Icons.photo_library),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// ignore_for_file: avoid_print, use_build_context_synchronously, deprecated_member_use, library_private_types_in_public_api
+
+import 'dart:async';
 import 'dart:io';
 import 'package:final_project/screens/SplashScreen.dart';
 import 'package:flutter/material.dart';
@@ -15,6 +229,7 @@ class ImagePickerScreen extends StatefulWidget {
 
 class _ImagePickerScreenState extends State<ImagePickerScreen> {
   File? _image;
+  bool _uploading = false; // Added _uploading variable
   final picker = ImagePicker();
 
   Future getImageFromCamera() async {
@@ -47,7 +262,13 @@ class _ImagePickerScreenState extends State<ImagePickerScreen> {
       return;
     }
 
-    final uri = Uri.parse("https://32f9-112-134-33-122.ngrok-free.app/upload");
+    // Show loading spinner for the upload button
+    setState(() {
+      _uploading = true;
+    });
+
+    final uri = Uri.parse(
+        "https://32f9-112-134-33-122.ngrok-free.app/upload"); // Replace with your server URL
     final request = http.MultipartRequest("POST", uri);
     request.files.add(await http.MultipartFile.fromPath('image', _image!.path));
 
@@ -58,6 +279,8 @@ class _ImagePickerScreenState extends State<ImagePickerScreen> {
       if (response.statusCode == 200) {
         // Handle success response
         print('Image uploaded successfully.');
+        // Navigate back to the same page after successful upload
+        Navigator.pop(context);
       } else {
         // Handle error response
         print('Error uploading image: ${response.body}');
@@ -65,6 +288,11 @@ class _ImagePickerScreenState extends State<ImagePickerScreen> {
     } catch (e) {
       // Handle any exceptions
       print('Exception uploading image: $e');
+    } finally {
+      // Hide loading spinner for the upload button
+      setState(() {
+        _uploading = false;
+      });
     }
   }
 
@@ -85,10 +313,12 @@ class _ImagePickerScreenState extends State<ImagePickerScreen> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  ElevatedButton(
-                    onPressed: uploadImage,
-                    child: const Text('Upload'),
-                  ),
+                  _uploading
+                      ? const CircularProgressIndicator() // Show loading spinner for the upload button
+                      : ElevatedButton(
+                          onPressed: uploadImage,
+                          child: const Text('Upload'),
+                        ),
                   const SizedBox(width: 10),
                   ElevatedButton(
                     onPressed: () {
